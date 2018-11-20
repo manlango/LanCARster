1a6f96c75544919e0b5997a040d0884f7b78f735
marina araceli nlang <manlangondo@Uh.edu>
2018-11-20 00:41:47 -0600

    some minor changes

--- db/migrate/20181119114839_create_employees.rb
+++ db/migrate/20181119114839_create_employees.rb
@@ -4,7 +4,7 @@ class CreateEmployees < ActiveRecord::Migration[5.2]
       t.string :first_name
       t.string :last_name
       t.string :email
-      t.references :role, foreign_key: true
+      t.references :department, foreign_key: true
 
       t.timestamps
     end
