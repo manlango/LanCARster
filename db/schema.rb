8a7aa13dc551f5f4b037906d6fecc9b1ba886d0d
marina araceli nlang <manlangondo@Uh.edu>
2018-11-20 21:35:10 -0600

    Admin can now assign roles to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_114839) do
+ActiveRecord::Schema.define(version: 2018_11_21_022023) do
 
   create_table "customers", force: :cascade do |t|
     t.string "gender"
@@ -70,6 +70,11 @@ ActiveRecord::Schema.define(version: 2018_11_19_114839) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.datetime "date_of_birth"
+    t.boolean "is_female", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
